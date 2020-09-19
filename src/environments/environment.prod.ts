6b7c73f236d03a42d32a553c9d2f9ccdcd806780
stephenc93 <stephenc93@gmail.com>
2020-09-18 21:12:49 -0400

    Alter prod environment variable

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  API_URL: `http://ec2-18-220-126-199.us-east-2.compute.amazonaws.com/`,
+  API_URL: `http://ec2-18-220-126-199.us-east-2.compute.amazonaws.com`,
   PORT: `8085`
 };
