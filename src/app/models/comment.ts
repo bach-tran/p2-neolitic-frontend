2381b41dd8f59c5e41d2065bf38f08059d9045e8
stephenc93 <stephenc93@gmail.com>
2020-09-21 21:51:13 -0400

    Refactor models to match backend and postcards to display post response

--- src/app/models/comment.ts
+++ src/app/models/comment.ts
@@ -1,6 +1,9 @@
+import { Time } from '@angular/common';
+import { User } from './user';
+
 export class Comment {
     private id: number;
 	private text: string;
-	private parentComment: number;
-	private post: number;
+	private author: User;
+	private timePosted: Time;
 }
--- src/app/postcard/postcard.component.ts
+++ src/app/postcard/postcard.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Postcard } from '../models/postcard';
 
 @Component({
   selector: 'app-postcard',
@@ -7,9 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PostcardComponent implements OnInit {
 
+  @Input() postcard: Postcard;
+  public imgSource: string;
+
   constructor() { }
 
   ngOnInit(): void {
+    this.imgSource = `http://ec2-18-220-126-199.us-east-2.compute.amazonaws.com:8085/neolitic/post/image/${this.postcard.id}`;
+    
   }
 
 }
