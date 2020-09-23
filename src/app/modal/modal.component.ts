8a94bd20ee42f989c8541610cd4ebb8fe8f89692
stephenc93 <stephenc93@gmail.com>
2020-09-22 20:29:13 -0400

    Begin adding getComments to modal component

--- src/app/modal/modal.component.ts
+++ src/app/modal/modal.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, Input, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { environment } from 'src/environments/environment';
+import { Comment } from '../models/comment';
 
 @Component({
   selector: 'app-modal',
@@ -7,9 +11,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ModalComponent implements OnInit {
 
-  constructor() { }
+  @Input() postId: number;
+  comments = [];
+
+  constructor(private http: HttpClient, private router: Router) { }
 
   ngOnInit(): void {
+    this.getComments();
   }
 
+  private async getComments(): Promise<Comment[]> {
+    const response = this.http.get<Comment[]>(environment.API_URL + ':' + environment.PORT + `/neolitic/comment?postId=${this.postId}`, {
+      withCredentials: true
+    }).toPromise();
+
+    this.setComments(await response);
+
+    return response;
+  }
+
+  public setComments(comments: Comment[]): void {
+    comments.sort((a, b) => (a.timePosted > b.timePosted) ? -1 : 1);
+
+    this.comments = comments;
+  }
 }
