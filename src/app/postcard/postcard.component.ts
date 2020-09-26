81e4d13b945a9dbb0e7d0c6275cfbc1b1b14a133
stephenc93 <stephenc93@gmail.com>
2020-09-26 13:09:18 -0400

    Add description to community card and remove btn for admins

--- src/app/postcard/postcard.component.ts
+++ src/app/postcard/postcard.component.ts
@@ -2,6 +2,7 @@ import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { Postcard } from '../models/postcard';
 import { environment } from 'src/environments/environment';
 import { Time } from '@angular/common';
+import { AuthenticationService } from '../services/authentication-service/authentication-service';
 
 @Component({
   selector: 'app-postcard',
@@ -12,14 +13,17 @@ export class PostcardComponent implements OnInit {
   public timePosted : Time;
   @Input() postcard: Postcard;
   public imgSource: string;
+  public role: string;
 
 
   @Output() postcardId: EventEmitter<number> = new EventEmitter();
 
-  constructor() { }
+  constructor(private authenticationService: AuthenticationService) { }
 
   ngOnInit(): void {
     this.imgSource = environment.API_URL + ':' + environment.PORT + `/neolitic/post/image/${this.postcard.id}`;
+
+    this.role = this.authenticationService.getUser().role.userRole;
   }
 
   emitPostcardId(): void {
